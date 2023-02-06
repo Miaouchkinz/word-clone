ed305ae0d811d60dd1872b16ef580cf6d3f14eb1
Gab <g.elodierichard@gmail.com>
2023-02-06 13:14:09 -0400

    updated GuessResults to show max guesses grid

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,14 @@
+import Guess from '../Guess/Guess';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import React from 'react';
+import { range } from '../../utils';
 
 function GuessResults({ guesses }) {
   return (
     <ol className="guess-results">
-      {guesses.length > 0 &&
-        guesses.map((guess, idx) => (
-          <li key={idx} className="guess">
-            {guess}
-          </li>
-        ))}
+      {range(NUM_OF_GUESSES_ALLOWED).map((guessIndex) => (
+        <Guess key={guessIndex} value={guesses[guessIndex]} />
+      ))}
     </ol>
   );
 }
